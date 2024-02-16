56c3f688f966bab2812cefc23afcf94a765947ce
KKS-Constant0fps <konstantin.s.kozlov@gmail.com>
2024-02-16 20:24:57 +0500

    some more

--- 14.02/c.py
+++ 14.02/c.py
@@ -4,13 +4,11 @@ b = set()
 for i in range(n):
     x, y = map(int, input().split(" "))
     if x != 0 and (y, -x) in a:
-        if (y, -x) not in b:
-            b.add((y,x))
-        a.remove((y,-x))
+        b.add((y,-abs(x)))
     else:
         a.add((y,x))
 b = list(b)
 b.sort()
 print(len(b))
 for elem in b:
-    print(f"({-abs(elem[1])}, {elem[0]}) - ({abs(elem[1])}, {elem[0]})")
+    print(f"({elem[1]}, {elem[0]}) - ({-elem[1]}, {elem[0]})")
